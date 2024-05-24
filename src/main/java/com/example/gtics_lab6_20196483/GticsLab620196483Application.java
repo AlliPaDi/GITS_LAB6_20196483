4ee32347ac02e374e9a30f9f2c3998ea19e7c7b1
Allison <a20196483@pucp.edu.pe>
2024-05-23 19:29:38 -0500

    first commit

---
+++ src/main/java/com/example/gtics_lab6_20196483/GticsLab620196483Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab6_20196483;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab620196483Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab620196483Application.class, args);
+    }
+
+}
